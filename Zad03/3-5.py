3ebfaf155325d3b58001914e6ac5f4e0ee707819
Jakub Opalinski <jakopal1125@gmail.com>
2023-10-28 23:29:39 +0200

    3-5

---
+++ Zad03/3-5.py
@@ -0,0 +1,39 @@
+
+def getMeasureTape(amount):
+
+    temp=[]
+
+    for i in range(amount):
+        temp.append("|")
+        temp.append("....")
+        if i==amount-1:
+            temp.append("|")
+
+    blankNumber=4
+
+
+    temp.append("\n")
+    for i in range(amount+1):
+        
+        temp.append(str(i))
+        if (i+1)%10==0 and (i)%9==0:
+           
+            blankNumber=blankNumber-1
+        spaceList = []
+        
+        for i in range(blankNumber):
+            spaceList.append(" ")
+        if i!=amount:
+            if(blankNumber!=0):
+                tempBlank="".join(spaceList)
+                temp.append(tempBlank);
+                
+    
+    measuringTape="".join(temp)
+    
+    return measuringTape
+
+
+print(getMeasureTape(12))
+    
+
