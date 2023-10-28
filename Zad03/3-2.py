f3458adb0084bbd2c1e9743a0c3f697d1461a16d
Jakub Opalinski <jakopal1125@gmail.com>
2023-10-28 20:37:05 +0200

    3-2

--- Zad03/3-2.py
+++ Zad03/3-2.py
@@ -1,13 +1,13 @@
 
 
-#L = [3, 5, 4] ; L = L.sort()
+L = [3, 5, 4] ; L = L.sort()
 
 #Podany kod przypisuje None do L zamiast posortrowanej listy
 #Aby zachowac posortowana liste nalezaloby to zrobic tak:
 #L = [3, 5, 4] ; L.sort()
 
 #---------------------------------------------------------------
-#x, y = 1, 2, 3
+x, y = 1, 2, 3
 
 #Podany kod próbuje się przypisać trzy wartości do dwóch zmiennych
 # To spowoduje błąd, ponieważ liczba wartości po prawej 
@@ -15,7 +15,7 @@
 
 #---------------------------------------------------------------
 
-#X = 1, 2, 3 ; X[1] = 4
+X = 1, 2, 3 ; X[1] = 4
 
 # Podany kod proboje utworzyć krotke,
 # Krotki  są niemutowalne, co oznacza,
@@ -23,13 +23,13 @@
 # próbujemy  zmienić wartość drugiego elementu krotki X, co powoduje błąd.
 #---------------------------------------------------------------
 
-#X = [1, 2, 3] ; X[3] = 4
+X = [1, 2, 3] ; X[3] = 4
 # Podany kod próbuje zmienić wartość elementu listy o indeksie 3 na 4.
 # Jednak lista nie zawiera indeksu 3 więc spowoduje to błąd.
 
 #---------------------------------------------------------------
 
-#X = "abc" ; X.append("d")
+X = "abc" ; X.append("d")
 
 #Podany kod tworzy łańcuch, łańcuchy sa niezmiennymi sekwencjami,
 #proba dodania d poprzez append generuje bład
@@ -38,12 +38,12 @@
 
 #---------------------------------------------------------------
 
-#L = list(map(pow, range(8)))
+L = list(map(pow, range(8)))
 
 # W podanym zamiast pow powinnismy umiesic lambde 
 # można rozwiazac to w taki sposób za pomoca lambdy:
-x=lambda i : pow(i,2)
-L = list(map(x, range(8)))
+#x=lambda i : pow(i,2)
+#L = list(map(x, range(8)))
 
 
 
