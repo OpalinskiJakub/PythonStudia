44716fd4c7dad7afb55790be54d6506219d13084
Jakub Opalinski <jakopal1125@gmail.com>
2023-10-28 15:42:03 +0200

    3-1.py

---
+++ Wyklad/Kolekcje/01.py
@@ -0,0 +1,11 @@
+list=[]
+
+for i in range(1,100):
+    list.append(i)
+
+x=0
+
+for i in list:
+    x+=i
+
+print(i)
--- Zad03/3-1.py
+++ Zad03/3-1.py
@@ -5,11 +5,17 @@ if (x > y):
 else:
     result = y;
 
-#
+#Podany kod jest poprawny składniowo w Pythonie i nie powinno być żadnych błędów. 
+# Python nie wymaga średników na końcu linii, 
+# ale można ich używać do oddzielenia instrukcji na jednej linii.
 
-for i in "axby": if ord(i) < 100: print (i)
+#for i in "axby": if ord(i) < 100: print (i)
 
 
 
-for i in "axby": print (ord(i) if ord(i) < 100 else i)
+for i in "axby": 
+    if ord(i) < 100: 
+        print(1)
+    else : 
+        print(1)
 
