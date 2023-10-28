54100cd4c2c89626726091a3303734ffd2d8be5d
Jakub Opalinski <jakopal1125@gmail.com>
2023-10-28 15:43:50 +0200

    3-1.py

--- Zad03/3-1.py
+++ Zad03/3-1.py
@@ -9,13 +9,25 @@ else:
 # Python nie wymaga średników na końcu linii, 
 # ale można ich używać do oddzielenia instrukcji na jednej linii.
 
-#for i in "axby": if ord(i) < 100: print (i)
+#----------------------------------------------------------------
 
 
+for i in "axby": if ord(i) < 100: print (i)
 
-for i in "axby": 
-    if ord(i) < 100: 
-        print(1)
-    else : 
-        print(1)
+#Podany kod ma błąd składniowy. W Pythonie, nie można używać instrukcji warunkowej if bez bloku, 
+#chyba że jest to (wyrażenie warunkowe(przy instrukcjach prostych można zrezygnować z wcięcia)). 
+#Poprawiony kod powinien wyglądać tak:
 
+for i in "axby":
+    if ord(i) < 100:
+        print(i)
+        
+#----------------------------------------------------------------
+
+for i in "axby": print (ord(i) if ord(i) < 100 else i)
+
+#Podany kod jest poprawny, korzystamy tutaj z wyrazenia warunkowego;
+# if condition: simple_statement
+# if condition to for i in "axby":
+# simple_statement to print (ord(i) if ord(i) < 100 else i)
+# ord(i) if ord(i) < 100 else i to wyrazenie trojargumentowe
