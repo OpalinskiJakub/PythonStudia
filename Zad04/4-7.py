d5944a015dcd1d7383e2ee87391d5b51dd348386
Jakub Opalinski <jakopal1125@gmail.com>
2023-11-05 10:11:09 +0100

    4-7

--- Zad04/4-7.py
+++ Zad04/4-7.py
@@ -12,5 +12,8 @@ input1=[(1,(2)),[1],[[[3,[3]]]]]
 result1=[1,2,1,3,3]
 input2=[[[[[[[1]]]]]],(23),((2),((4)))]
 result2=[1,23,2,4]
+input3=[1,(2,3),[],[4,(5,6,7)],8,[9]]
+result3=[1,2,3,4,5,6,7,8,9]
 assert flatten(input1)==result1,f"Expected:{result1}"
 assert flatten(input2)==result2,f"Expected:{result2}"
+assert flatten(input3)==result3,f"Expected:{result3}"
